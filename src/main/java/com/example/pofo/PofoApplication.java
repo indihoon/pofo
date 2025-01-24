b4a8d5116c95a653b2dc9aa8bb99414968d37020
kimdh <indihoon@naver.com>
2025-01-24 15:18:51 +0900

    start

---
+++ src/main/java/com/example/pofo/PofoApplication.java
@@ -0,0 +1,13 @@
+package com.example.pofo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PofoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PofoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/pofo/PofoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.pofo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PofoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
