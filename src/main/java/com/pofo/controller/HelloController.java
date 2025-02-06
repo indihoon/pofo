4ed559021e6d2949393d27dff6c86f8d59f224cb
kimdh <indihoon@naver.com>
2025-02-06 14:55:08 +0900

    proxy

--- src/main/java/com/pofo/controller/HelloController.java
+++ src/main/java/com/pofo/controller/HelloController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 //@CrossOrigin(origins = "http://localhost:3000")
-@Controller
+@RestController
 @RequestMapping("/home")
 public class HelloController {
     @GetMapping("/message")
